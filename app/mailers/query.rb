fa799c0a66eb0a83bc502a93cc35a4788647af8b
aasciesh <bhattarai.asish@gmail.com>
2014-07-25 15:31:04 +0300

    Update query.rb

--- app/mailers/query.rb
+++ app/mailers/query.rb
@@ -6,4 +6,8 @@ class Query < ActionMailer::Base
   	@input = input
    	mail(to: 'getaasciesh@hotmail.com', subject: 'New happenly.com query has arrived.')
   end
+  def error(input)
+    @input = input
+   	mail(to: 'getaasciesh@hotmail.com', subject: 'New error from happenly.com has arrived.')
+  end
 end
