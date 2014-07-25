899d700190f8eaaa09a2213d6abe22f1ed92cf9b
aasciesh <bhattarai.asish@gmail.com>
2014-07-25 15:29:51 +0300

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,13 +6,15 @@ class HomeController < ApplicationController
 		if input_valid?(params[:query])
 			Query.query_email(params[:query]).deliver
 			@message = "Thank you for querying."
-			render 'index'
 		else
 			@message = "Something went wrong. Did you fill all fields?"
-			render 'index'
 		end
+		render 'index'
  	end
-
+	def error
+		Query.error(params).deliver
+		render 'index'
+	end
  	private
  	def input_valid?(input)
  		!(input[:full_name].length < 1 || input[:email].length < 1 || input[:price].length < 1 || input[:message].length < 1)
